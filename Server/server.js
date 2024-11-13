06b1ba298d3ddb695b6bf7f34163b781d69ed6ee
Rivin Jayawardana <127378095+RivinJayawardhana@users.noreply.github.com>
2024-11-13 09:46:14 +0530

    First

---
+++ Server/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB')
+}).catch((err)=>{
+    console.log(err)
+});
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+  };
+
+app.use(cors(corsOptions));
+
+app.listen(3000 ,() =>{
+    console.log('Server Listning on port 3000')
+});
+
+
+
+
+
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode
+    });
+})
