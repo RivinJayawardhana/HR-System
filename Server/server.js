e088a5148c3bd19ee6bb1668fe8a9bbd86e1fbd7
Rivin Jayawardana <127378095+RivinJayawardhana@users.noreply.github.com>
2024-11-13 12:29:20 +0530

    server done

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect("mongodb+srv://rivin:rivin1234@cluster0.48fyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to MongoDB')
 }).catch((err)=>{
     console.log(err)
